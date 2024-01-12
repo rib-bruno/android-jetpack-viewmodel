8d1126375994a514a7323c509af1b1cce8f14506
rib-bruno <ribeirobrunosouza@hotmail.com>
2024-01-12 17:25:53 -0300

    finalizando o aplicativo - funcional

--- app/src/main/java/me/dio/urlshortener/di/DataLocalModule.kt
+++ app/src/main/java/me/dio/urlshortener/di/DataLocalModule.kt
@@ -5,6 +5,7 @@ import androidx.room.Room
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import me.dio.urlshortener.data.AppDatabase
 import me.dio.urlshortener.data.dao.ShortenedUrlDao
@@ -16,8 +17,7 @@ import javax.inject.Singleton
 class DataLocalModule {
     @Provides
     @Singleton //mesma referência para todos
-    fun provideDatabase(context: Context) : AppDatabase =
-        //TODO - CRIAR O APPDATABASE
+    fun provideDatabase(@ApplicationContext context: Context) : AppDatabase =
         Room.databaseBuilder(context, AppDatabase::class.java, "app.db")
             .build()
 
